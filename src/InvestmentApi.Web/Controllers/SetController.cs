b0ec8e9835615bc1e0c431e3503f92272da013b8
Soul_Master <Soul_Master@live.com>
2014-08-18 00:51:06 +0700

    Add Set/DelistedCompanies and Set/DelistedCompanySymbols api.

--- src/InvestmentApi.Web/Controllers/SetController.cs
+++ src/InvestmentApi.Web/Controllers/SetController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -46,7 +47,7 @@ namespace InvestmentApi.Web.Controllers
         }
 
         /// <summary>
-        /// สัญลักษณ์บริษัทจดทะเบียนในตลาดหลักทรัพย์ (ปรับปรุงข้อมูลทุกวันทำการแรกของแต่ละสัปดาห์)
+        /// สัญลักษณ์ของบริษัทจดทะเบียนในตลาดหลักทรัพย์ (ปรับปรุงข้อมูลทุกวันทำการแรกของแต่ละสัปดาห์)
         /// </summary>
         [HttpGet]
         [Route("ListedCompanySymbols")]
@@ -64,8 +65,61 @@ namespace InvestmentApi.Web.Controllers
                 Content = new StringContent(result, Encoding.UTF8, "text/plain")
             };
         }
+
+        /// <summary>
+        /// รายชื่อบริษัทจดทะเบียนที่ถูกเพิกถอนในตลาดหลักทรัพย์
+        /// </summary>
+        [HttpGet]
+        [Route("DelistedCompanies")]
+        public IEnumerable<DelistedCompanyInfo> DelistedCompanies()
+        {
+            var dom = CsQueryHelpers.GetUrl("http://www.set.or.th/listedcompany/static/delistedSecurities_th_TH.xls", Encoding.GetEncoding("tis-620"));
+            var rows = dom.Select("tr");
+            var thaiCulture = new CultureInfo("th-TH");
+
+            for (var i = 2; i < rows.Length; i++)
+            {
+                var td = CQ.Create(rows[i]).Select("td");
+                var model = new DelistedCompanyInfo
+                {
+                    Symbol = td[0].Text(),
+                    Name = td[1].Text(),
+                    FirstTradingDate = DateTime.ParseExact(td[2].Text(), "dd MMM yyyy", thaiCulture),
+                    DelistedDate = DateTime.ParseExact(td[3].Text(), "dd MMM yyyy", thaiCulture),
+                    MarketName = td[4].Text(),
+                    IndustryGroup = td[5].Text(),
+                    BusinessSector = td[6].Text(),
+                    DelistedReason = td[7].Text()
+                };
+
+                yield return model;
+            }
+        }
+
+        /// <summary>
+        /// สัญลักษณ์ของบริษัทจดทะเบียนที่ถูกเพิกถอนในตลาดหลักทรัพย์
+        /// </summary>
+        [HttpGet]
+        [Route("DelistedCompanySymbols")]
+        public HttpResponseMessage DelistedCompanySymbols()
+        {
+            var result = DelistedCompanies().Select(x => x.Symbol).Aggregate(string.Empty, (current, item) =>
+            {
+                if (current == string.Empty) return item;
+
+                return current + Environment.NewLine + item;
+            });
+
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(result, Encoding.UTF8, "text/plain")
+            };
+        }
     }
 
+    /// <summary>
+    ///  ข้อมูลบริษัทจดทะเบียนในตลาดหลักทรัพย์
+    /// </summary>
     public class CompanyInfo
     {
         public string Symbol { get; set; }
@@ -78,5 +132,20 @@ namespace InvestmentApi.Web.Controllers
         public string TelephoneNo { get; set; }
         public string FaxNo { get; set; }
         public string WebSite { get; set; }
+    }
+
+    /// <summary>
+    ///  ข้อมูลบริษัทที่ถูกเพิกถอนในตลาดหลักทรัพย์
+    /// </summary>
+    public class DelistedCompanyInfo
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string MarketName { get; set; }
+        public string IndustryGroup { get; set; }
+        public string BusinessSector { get; set; }
+        public DateTime FirstTradingDate { get; set; }
+        public DateTime DelistedDate { get; set; }
+        public string DelistedReason { get; set; }
     }
 }
